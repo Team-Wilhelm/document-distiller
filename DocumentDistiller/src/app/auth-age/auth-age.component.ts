fde17c2c4272c78bbc0eb68d64aa410b547fda5a
Julia Ilasova <1julka1il@gmail.com>
2024-03-06 19:34:47 +0100

    update angular, add Flowbite UI library and generate basic pages

---
+++ DocumentDistiller/src/app/auth-age/auth-age.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-age',
+  standalone: true,
+  imports: [],
+  templateUrl: './auth-age.component.html',
+})
+export class AuthAgeComponent {
+
+}
---
+++ DocumentDistiller/src/app/file-upload-page/file-upload-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-file-upload-page',
+  templateUrl: './file-upload-page.component.html',
+})
+export class FileUploadPageComponent {
+
+}
