b3b082b890d781c93ce0e10ba0a98c6410c8e9dc
mariaruth1 <113031776+mariaruth1@users.noreply.github.com>
2024-03-07 21:15:43 +0100

    who even knows anymore?

--- Api/Controller/DocumentController.cs
+++ Api/Controller/DocumentController.cs
@@ -1,4 +1,6 @@
+using System.Text;
 using Core;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace VirtualFriend.Controller;
@@ -6,21 +8,52 @@ namespace VirtualFriend.Controller;
 public class DocumentController(DocumentService documentService) : ControllerBase
 {
     [HttpPost("summarise")]
-    public async Task<IActionResult> SummariseDocument(IFormFile file)
+    public async Task<IActionResult> SummariseDocument(IFormFile? file)
     {
         if (file == null || file.Length == 0)
         {
             return BadRequest("File is null or empty");
         }
-
-        Console.WriteLine($"Received file: {file.FileName}");
-        return Ok();
+        
+        //TODO: find a way to convert file to string properly, does this even work? Also move this to the service probably
+        var fileString = ConvertFileToString(file);
+        var summarisedDocument = await documentService.SummariseContent(fileString);
+        
+        //TODO: decide on the return type
+        return Ok(summarisedDocument);
+        
+        //Console.WriteLine($"Received file: {file.FileName}");
     }
-    
+
+    [HttpPost("keypoints")]
+    public async Task<IActionResult> ExtractKeyPoints(IFormFile? file)
+    {
+        if (file == null || file.Length == 0)
+        {
+            return BadRequest("File is null or empty");
+        }
+        var fileString = ConvertFileToString(file);
+        var keyPoints = await documentService.ExtractKeyPoints(fileString);
+        
+        return Ok(keyPoints);
+    }
+
     [HttpPost("translate")]
     public async Task<IActionResult> TranslateDocument([FromBody] string message)
     {
         //return documentService.ExtractKeyPoints(message);
         throw new NotImplementedException();
+    }
+    
+    //TODO: move this to utils if it will be used fr fr
+    public string ConvertFileToString(IFormFile file)
+    {
+        var fileString = new StringBuilder();
+        using (var reader = new StreamReader(file.OpenReadStream()))
+        {
+            while (reader.Peek() >= 0)
+                fileString.AppendLine(reader.ReadLine()); 
+        }
+        return fileString.ToString();
     }
 }
--- Core/DocumentService.cs
+++ Core/DocumentService.cs
@@ -1,14 +1,79 @@
+using System.Text;
+using Azure;
+using Azure.AI.TextAnalytics;
+
 namespace Core;
 
-public class DocumentService
+public class DocumentService(TextAnalyticsClient client)
 {
-    public async Task<string> SummariseCoument(string message)
+    public async Task<string?> SummariseContent(string message)
     {
-        throw new NotImplementedException();
+        var summaryResult = new StringBuilder();
+        
+        var batchInput = new List<string>
+        {
+            message
+        };
+        
+        var actions = new TextAnalyticsActions
+        {
+            ExtractiveSummarizeActions = new List<ExtractiveSummarizeAction> { new() },
+            //AbstractiveSummarizeActions = new List<AbstractiveSummarizeAction> { new() }
+        };
+        
+        // Start analysis process.
+        var operation = await client.StartAnalyzeActionsAsync(batchInput, actions);
+        await operation.WaitForCompletionAsync();
+        
+        await foreach (var documentsInPage in operation.Value)
+        {
+            IReadOnlyCollection<ExtractiveSummarizeActionResult> summaryResults = documentsInPage.ExtractiveSummarizeResults;
+            //IReadOnlyCollection<AbstractiveSummarizeActionResult> summaryResults = documentsInPage.AbstractiveSummarizeResults;
+        
+            foreach (var summaryActionResults in summaryResults)
+            {
+                if (summaryActionResults.HasError)
+                {
+                    Console.WriteLine("  Error!");
+                    Console.WriteLine($"  Action error code: {summaryActionResults.Error.ErrorCode}.");
+                    Console.WriteLine($"  Message: {summaryActionResults.Error.Message}");
+                    continue;
+                }
+        
+                foreach (var documentResults in summaryActionResults.DocumentsResults)
+                {
+                    if (documentResults.HasError)
+                    {
+                        Console.WriteLine("  Error!");
+                        Console.WriteLine($"  Document error code: {documentResults.Error.ErrorCode}.");
+                        Console.WriteLine($"  Message: {documentResults.Error.Message}");
+                        continue;
+                    }
+        
+                    foreach (var sentence in documentResults.Sentences)
+                    {
+                        summaryResult.AppendLine(sentence.Text);
+                    }
+                }
+            }
+        }
+        return summaryResult.ToString();
     }
 
-    public async Task ExtractKeyPoints(string message)
+    public async Task<string> ExtractKeyPoints(string message)
     {
-        throw new NotImplementedException();
+        var keyPhrasesString = "";
+        try
+        {
+            var response = await client.ExtractKeyPhrasesAsync(message);
+            var keyPhrases = response.Value;
+            keyPhrasesString = string.Join(", ", keyPhrases);
+        }
+        catch (RequestFailedException exception)
+        {
+            Console.WriteLine($"Error Code: {exception.ErrorCode}");
+            Console.WriteLine($"Message: {exception.Message}");
+        }
+        return keyPhrasesString;
     }
 }
